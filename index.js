930807caf4b73e77ee81c627754c20595ed12d48
Nicolai Gorden <nicolaigorden@gmail.com>
2021-10-03 15:58:57 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let cardArray = [];
+    for (let i = 0; i < names.length; i++) {
+        let cardText = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        cardArray.push(cardText);
+        debugger;
+    }
+return cardArray
+}
+
+function countDown(int = 0) {
+    while (int > -1) {
+      console.log(int--);
+    }
+  }
